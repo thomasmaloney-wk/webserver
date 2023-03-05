3c8e6165513cdf7c6f8a49d96dbb571f6bc5efc2
thomasmaloney-wk <thomas.maloney@workiva.com>
2023-03-05 14:37:28 -0600

    moved response codes into enum

--- http/http_response.cpp
+++ http/http_response.cpp
@@ -5,31 +5,22 @@ const http_response *
 http_response::method_not_allowed(std::vector<std::string> allowed_methods) {
   const std::string body =
       "<html><body><h1>405 Method Not Allowed</h1></body></html>";
-  return new http_response(http_response::METHOD_NOT_ALLOWED, body,
+  return new http_response(http_response_code::METHOD_NOT_ALLOWED, body,
                            "text/html");
 }
 
 const http_response *http_response::not_found(const std::string &body_text) {
-  return new http_response(http_response::NOT_FOUND, body_text, "text/html");
+  return new http_response(http_response_code::NOT_FOUND, body_text,
+                           "text/html");
 }
 
-http_response::http_response(int status_code, const std::string &body,
+http_response::http_response(http_response_code status_code,
+                             const std::string &body,
                              const std::string &content_type)
     : status_code(status_code), body(body), content_type(content_type) {
   // Set default headers
   headers["Content-Length"] = std::to_string(body.size());
   headers["Content-Type"] = content_type;
-}
-
-std::string http_response::to_string_deprecated() const {
-  std::ostringstream oss;
-  oss << "HTTP/1.1 " << status_code << " " << status_code_to_string(status_code)
-      << "\r\n";
-  oss << "Content-Type: " << content_type << "\r\n";
-  oss << "Content-Length: " << body.length() << "\r\n";
-  oss << "\r\n";
-  oss << body;
-  return oss.str();
 }
 
 void http_response::add_header(std::string key, std::string value) {
@@ -49,23 +40,24 @@ std::string http_response::to_string() const {
   return oss.str();
 }
 
-std::string http_response::status_code_to_string(int status_code) {
+std::string
+http_response::status_code_to_string(http_response_code status_code) {
   switch (status_code) {
-  case http_response::OK:
+  case http_response_code::OK:
     return "OK";
-  case http_response::CREATED:
+  case http_response_code::CREATED:
     return "Created";
-  case http_response::NO_CONTENT:
+  case http_response_code::NO_CONTENT:
     return "No Content";
-  case http_response::BAD_REQUEST:
+  case http_response_code::BAD_REQUEST:
     return "Bad Request";
-  case http_response::UNAUTHORIZED:
+  case http_response_code::UNAUTHORIZED:
     return "Unauthorized";
-  case http_response::FORBIDDEN:
+  case http_response_code::FORBIDDEN:
     return "Forbidden";
-  case http_response::NOT_FOUND:
+  case http_response_code::NOT_FOUND:
     return "Not Found";
-  case http_response::INTERNAL_SERVER_ERROR:
+  case http_response_code::INTERNAL_SERVER_ERROR:
     return "Internal Server Error";
   default:
     return "Unknown Status Code";
--- http/http_response.h
+++ http/http_response.h
@@ -1,35 +1,38 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+enum http_response_code {
+  OK = 200,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  INTERNAL_SERVER_ERROR = 500
+};
 class http_response {
 public:
-  static const int OK = 200;
-  static const int CREATED = 201;
-  static const int NO_CONTENT = 204;
-  static const int BAD_REQUEST = 400;
-  static const int UNAUTHORIZED = 401;
-  static const int FORBIDDEN = 403;
-  static const int NOT_FOUND = 404;
-  static const int METHOD_NOT_ALLOWED = 405;
-  static const int INTERNAL_SERVER_ERROR = 500;
+  static inline std::string SHUTDOWN_HEADER_KEY = "Hacky-Shutdown-Override";
 
   static const http_response *
   method_not_allowed(std::vector<std::string> allowed_methods);
   static const http_response *
   not_found(const std::string &body_text = "404 Not Found");
 
-  int status_code;
+  http_response_code status_code;
   std::string body;
   std::string content_type;
   std::unordered_map<std::string, std::string> headers;
 
-  http_response(int status_code, const std::string &body,
+  http_response(http_response_code status_code, const std::string &body,
                 const std::string &content_type = "text/plain");
   std::string to_string() const;
-  std::string to_string_deprecated() const;
 
   void add_header(std::string key, std::string value);
 
 private:
-  static std::string status_code_to_string(int status_code);
+  static std::string status_code_to_string(http_response_code status_code);
 };
