42cc7b58522482fca194caefd067e83d07bd2335
thomasmaloney-wk <thomas.maloney@workiva.com>
2023-03-05 17:09:35 -0600

    shutdown controller now handles post requests and does not shut down server on get requests

--- controllers/shutdown_controller.cpp
+++ controllers/shutdown_controller.cpp
@@ -3,13 +3,24 @@
 #include "../http/http_request.h"
 #include "../http/http_response.h"
 
-http_response *shutdown_controller::home() { 
+http_response *shutdown_controller::home() {
   const auto response_body = load_file("web/shutdown.html");
   return new http_response(http_response_code::OK, response_body, "text/html");
 }
 
-http_response *shutdown_controller::shutdown() { return nullptr; }
+http_response *shutdown_controller::shutdown() {
+  auto response =
+      new http_response(http_response_code::OK, "Shut down recieved");
+  response->add_header(http_response::SHUTDOWN_HEADER_KEY, "true");
+  return response;
+}
 
 http_response *shutdown_controller::handle_request(http_request *request) {
-  return nullptr;
+  if (request->method() == "GET") {
+    return home();
+  }
+  if (request->method() == "POST") {
+    return shutdown();
+  }
+  return (http_response *)http_response::method_not_allowed({"GET"});
 }
